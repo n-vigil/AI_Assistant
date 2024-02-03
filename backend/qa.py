75ab7c4e0832421e5ecdcf784df255ff6eeb1ee5
Nancy <nvigil08@gmail.com>
2024-02-03 15:22:15 -0800

    open ai mods

--- backend/qa.py
+++ backend/qa.py
@@ -1,13 +1,15 @@
 OPEN_API_KEY = "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"
 
+import langchain
 from langchain.llms import OpenAI
 llm = OpenAI(openai_api_key = OPEN_API_KEY)
 
-# kintone REST API requests
-@domain = nancy-v@kintone.com
-@appId = 3
-@apiToken = ybpuAL6wTujk1lczjWu8Q1ojLlxQAWMdvet7Sjyi
-
-### GET
-GET https://{{domain}}/k/v1.records.json?app={{appId}}
-X-Cybozu-API-Token: {{apiToken}}
+first_question = True
+while True:
+    if first_question:
+        intro_text = input("\nEnter your question or type 'quit' to exit!")
+        first_question = False
+    else:
+        intro_text = input("\n Any more questions?")
+    if intro_text.lower == 'quit':
+        break
