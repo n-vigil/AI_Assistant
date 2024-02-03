641ab34852e30b8512571bc283694c73cdfce87f
Nancy <nvigil08@gmail.com>
2024-02-03 14:37:12 -0800

    initial kintone api

--- backend/qa.py
+++ backend/qa.py
@@ -2,3 +2,8 @@ OPEN_API_KEY = "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"
 
 from langchain.llms import OpenAI
 llm = OpenAI(openai_api_key = OPEN_API_KEY)
+
+# kintone REST API requests
+@domain = nancy-v@kintone.com
+@appId = 3
+@apiToken = ybpuAL6wTujk1lczjWu8Q1ojLlxQAWMdvet7Sjyi
