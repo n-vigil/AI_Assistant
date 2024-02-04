566995b0d9f88607636db961a8cd046bc2f1c0e7
Nancy <nvigil08@gmail.com>
2024-02-04 01:03:00 -0800

    integrate qa into main

--- backend/main.py
+++ backend/main.py
@@ -3,19 +3,21 @@ from flask import Flask, request, jsonify
 import os
 import openai
 from openai import OpenAI
-from utils import create_assistant
 from flask_cors import CORS, cross_origin
-from backend/qa.py import run_script
+import qa
 
 app = Flask(__name__)
 
 client = OpenAI(api_key = "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")
 
 @app.route('/')
-return 'Hello World'
+def index():
+  return 'Hello World'
 
-@app.route('/bot', methods = [GET, POST])
-run_script()
+@app.route('/bot', methods = ["POST"])
+def test_():
+  qa.run_script()
+  return
   
 
 
--- backend/qa.py
+++ backend/qa.py
@@ -1,9 +1,11 @@
 import openai
+from openai import OpenAI
 
 
 def run_script():
-    OPENAI_API_KEY = "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"
+    OPENAI_API_KEY = "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"
     openai.api_key = OPENAI_API_KEY
+    client = OpenAI(api_key = "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")
 
     first_question = True
     while True:
@@ -17,7 +19,7 @@ def run_script():
             break
 
     # Use the OpenAI API to generate a response with the chat model
-        response = openai.ChatCompletion.create(
+        response = openai.client.chat.completions.create(
             model="gpt-3.5-turbo-16k",
             messages=[
                 {"role": "system", "content": "You serve as a helpful assistant, specializing in providing users with information on volunteering opportunities and food banks. Users can inquire about available volunteering positions, receive details on specific organizations, and obtain contact information. Similarly, the chatbot assists in locating nearby food banks, offering addresses and contact details. It also gives government resources to assist in food insecurity. To maintain focus, if users pose unrelated questions, the chatbot gently redirects them to its primary role of providing valuable information about volunteering opportunities and food banks. This ensures a seamless and efficient user experience centered around community engagement and assistance.  A question with no location will be followed up by the question 'What is the location that you are searching for resources' The user may answer with an address, city, or zip code."},
@@ -29,3 +31,4 @@ def run_script():
         answer = response['choices'][0]['message']['content'].strip()
 
         print("ANSWER: \"%s\"\n" % answer)
+run_script()
