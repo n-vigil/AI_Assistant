877383e33b68f3233a23c287918b3a72ba40a11a
Nancy <nvigil08@gmail.com>
2024-02-03 20:06:32 -0800

    clean version

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,6 @@
 print("hello world!")
-# from openai import OPENAI
 from flask import Flask
 import os
-
-# client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", "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"))
-
-
 
 app = Flask(__name__)
 
@@ -13,18 +8,9 @@ app = Flask(__name__)
 def index():
   return 'Hello World'
 
-  # Example OpenAI Python library request
-# MODEL = "gpt-3.5-turbo"
-# response = client.chat.completions.create(
-#     model=MODEL,
-#     messages=[
-#         {"role": "system", "content": "You are a helpful assistant."},
-#         {"role": "user", "content": "Knock knock."},
-#         {"role": "assistant", "content": "Who's there?"},
-#         {"role": "user", "content": "Orange."},
-#     ],
-#     temperature=0,
-# )
+@app.route('/bot')
+def test():
+    return "Cash Money"
 
 
 if __name__ == '__main__':
