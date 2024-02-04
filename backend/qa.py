d0d8abf52fd95c64a888b13228ea710ccd7ee27a
Nancy <nvigil08@gmail.com>
2024-02-03 16:01:50 -0800

    add input prompt

--- backend/qa.py
+++ backend/qa.py
@@ -1,15 +1,19 @@
 OPEN_API_KEY = "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"
 
 import langchain
-from langchain.llms import OpenAI
+import langchain_community
+from langchain_community.llms import OpenAI
 llm = OpenAI(openai_api_key = OPEN_API_KEY)
 
 first_question = True
 while True:
     if first_question:
-        intro_text = input("\nEnter your question or type 'quit' to exit!")
+        intro_text = input("\nEnter your question or type 'quit' to exit!\n")
         first_question = False
     else:
-        intro_text = input("\n Any more questions?")
-    if intro_text.lower == 'quit':
+        intro_text = input("\n Any more questions?\n")
+
+    if intro_text.lower == "quit":
         break
+
+    print("QUESTION: \"%s\"" % intro_text)
